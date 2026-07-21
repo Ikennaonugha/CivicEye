82dd5c1fb3eddbbc377f93a5157763dcd4056971
Ikenna Onugha <i.onugha@alustudent.com>
2026-07-21 17:42:29 +0100

    fix/signup n login

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,4 @@
 from django.views.generic import TemplateView
-from django.contrib import messages
-from django.urls import reverse_lazy
-from django.views.generic import FormView
-from .forms import ContactForm
 
 class HomePageView(TemplateView):
     template_name = "home.html"
