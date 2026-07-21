a3d8e12be0f31872ebc61bc291de4746b754e961
Ikenna Onugha <i.onugha@alustudent.com>
2026-07-21 17:08:54 +0100

    fix/scrapped contact form

--- pages/views.py
+++ pages/views.py
@@ -9,25 +9,3 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
-
-
-
-class ContactView(FormView):
-    template_name = 'home.html'       # Your template path
-    form_class = ContactForm
-    success_url = reverse_lazy('home')
-
-    def form_valid(self, form):
-        # Access validated data from form.cleaned_data
-        full_name = form.cleaned_data['fullName']
-        email = form.cleaned_data['email']
-        phone = form.cleaned_data['phone']
-        message_text = form.cleaned_data['message']
-
-        # Handle the message (e.g., send an email or log it)
-        # send_mail(...)
-
-        # Add success message
-        messages.success(self.request, "Your message has been sent successfully!")
-        
-        return super().form_valid(form)
